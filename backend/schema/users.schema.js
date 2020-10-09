3e22af2954ca03b3785c8acebc1d79749ac466f9
Eddie Saunders <saunders.eddie@outlook.com>
2020-10-09 06:44:47 -0400

    Added user schema into schema server folder

---
+++ backend/schema/users.schema.js
@@ -0,0 +1,25 @@
+// Basic user schema, we'll investiaget passwords later on.
+// edwyn.saunders@outlook.com 9th Oct 2020
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
